22abfea4398f931ea4a3cfadb9d19a47c0a1844a
aflahur <aflahurrofik@gmail.com>
2023-06-23 16:29:51 +0700

    change description file

--- newTest.js
+++ newTest.js
@@ -1,13 +1,14 @@
-const request = require("supertest")("https://reqres.in")
-const expect = require("chai").expect;
+//this is my previous test, i try another one on test.js
+// const request = require("supertest")("https://reqres.in")
+// const expect = require("chai").expect;
 
-describe("POST User REQRES", function () {
-    it("Success Register", async function () {
-        const response = await request
-            .post("/api/users")
-            .send({ name: "aflahur", job: "QAengineer" });
+// describe("POST User REQRES", function () {
+//     it("Success Register", async function () {
+//         const response = await request
+//             .post("/api/users")
+//             .send({ name: "aflahur", job: "QAengineer" });
 
-        expect(response.body.name).to.eql('aflahur')
-        expect(response.body.job).to.eql('QAengineer')
-    })
-})
+//         expect(response.body.name).to.eql('aflahur')
+//         expect(response.body.job).to.eql('QAengineer')
+//     })
+// })
